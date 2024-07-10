b6d45b435efe4b5a8f62966377168c7fa88b13a9
GelsonJRodrigues <gelson.jose.rodrigues@gmail.com>
2024-07-10 11:33:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    return res.send("<h3>Rotas no Express</h3><p>Rota '/'")
+})
+
+app.post('/', (req, res) => {
+    return res.status(200).json('Rota POST')
+})
+
+app.put('/', (req, res) => {
+    return res.status(200).json('Rota PUT')
+})
+
+app.delete('/', (req, res) => {
+    return res.status(200).json('Rota DELETE')
+})
+
+app.listen(8080, () =>
+    console.log('Servidor iniciado na porta 3000')
+);
